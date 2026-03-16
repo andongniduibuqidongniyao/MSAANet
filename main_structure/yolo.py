822f06cf08c787ffb78377dfc3bce43031452715
andongniduibuqidongniyao <sucheng411522@163.com>
2026-03-16 13:12:55 +0800

    Update yolo.py

--- main_structure/yolo.py
+++ main_structure/yolo.py
@@ -64,7 +64,6 @@ class YoloBody(nn.Module):
 		base_depth = max(round(dep_mul * 3), 1)
 		self.backbone = Backbone(base_channels, base_depth, deep_mul, phi, pretrained=pretrained)
 		
-		# ------------------------加强特征提取网络------------------------#
 		self.upsample = CARAFE(base_channels * 8, base_channels * 8)
 		# self.upsample_1 = EUCB(base_channels * 8, base_channels * 8)
 		# 1024 * deep_mul + 512, 40, 40 => 512, 40, 40
@@ -115,37 +114,17 @@ class YoloBody(nn.Module):
 		P5_upsample = self.upsample(feat3)
 		# print(f"P5_upsample_shape{P5_upsample.shape}")
 		# print(f"feat2_shape{feat2.shape}")
-		# 1024 * deep_mul, 40, 40 cat 512, 40, 40 => 1024 * deep_mul + 512, 40, 40 --> 1024, 40, 40
 		P4 = torch.cat([P5_upsample, feat2], 1)
-		# 1024 * deep_mul + 512, 40, 40 == 1024, 40, 40 => 512, 40, 40
 		P4 = self.conv3_for_upsample1(P4)
-		
-		# 512, 40, 40 => 512, 80, 80
 		P4_upsample = self.upsample(P4)
-		
-		# 512, 80, 80 cat 256, 80, 80 => 768, 80, 80
 		P3 = torch.cat([P4_upsample, feat1], 1)
-		
-		# 768, 80, 80 => 256, 80, 80
 		P3 = self.conv3_for_upsample2(P3)
-		
-		# 256, 80, 80 => 256, 40, 40
 		P3_downsample = self.down_sample1(P3)
-		
-		# 512, 40, 40 cat 256, 40, 40 => 768, 40, 40
 		P4 = torch.cat([P3_downsample, P4], 1)
-		
-		# 768, 40, 40 => 1280, 40, 40
 		P4 = torch.cat([P4, feat2], 1)
-		# 1280, 40, 40 => 512, 40, 40
 		P4 = self.conv3_for_downsample1(P4)
-		
-		# 512, 40, 40 => 512, 20, 20
 		P4_downsample = self.down_sample2(P4)
-		
-		# 512, 20, 20 cat 1024 * deep_mul, 20, 20 => 1024 * deep_mul + 512, 20, 20
 		P5 = torch.cat([P4_downsample, feat3], 1)
-		# 1024 * deep_mul + 512, 20, 20 => 1024 * deep_mul, 20, 20
 		P5 = self.conv3_for_downsample2(P5)
 		shape = P3.shape  # BCHW
 		return [P3, P4, P5, shape]
